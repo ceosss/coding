254aee4ee754b15c76468fa1dec8438b9d6bdb18
ceosss <sswarajsamant@gmail.com>
2020-06-13 00:35:35 +0530

    Input and Output

---
+++ Hackerrank Practice/C++/Input and Output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << x + y + z;
+    return 0;
+}
