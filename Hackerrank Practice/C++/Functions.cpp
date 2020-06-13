84fa07b237cdbd0750b0f6c3f3ec299263a41c5e
ceosss <sswarajsamant@gmail.com>
2020-06-13 22:37:55 +0530

    Functions

---
+++ Hackerrank Practice/C++/Functions.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int z = max(a, max(b, max(c, d)));
+    return z;
+}
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
