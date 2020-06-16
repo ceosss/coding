270d07ef0e0e9b3043840c3a490c45dff7cb46fd
ceosss <sswarajsamant@gmail.com>
2020-06-16 22:06:30 +0530

    Vector-Sort

---
+++ Hackerrank Practice/C++/Vector-Sort.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    sort(a.begin(), a.end());
+    for (auto i : a)
+        cout << i << " ";
+    return 0;
+}
