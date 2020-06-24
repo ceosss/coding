0a344c6d0bac61297c1d2cd411bd3aad1c68d635
ceosss <sswarajsamant@gmail.com>
2020-06-24 23:38:29 +0530

    Stones on the Table

---
+++ CodeForces Practice/Stones on the Table.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int count = 0;
+    for (int i = 0; i < s.length() - 1; i++)
+    {
+        if (s[i] == s[i + 1])
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
