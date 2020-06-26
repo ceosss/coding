727af348013ebb3f92df6e3e13efd26cd2a4c36f
ceosss <sswarajsamant@gmail.com>
2020-06-26 21:06:55 +0530

    George and Accomodation

---
+++ CodeForces Practice/George and Accomodation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int p, q;
+        cin >> p >> q;
+        if (q - p >= 2)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
