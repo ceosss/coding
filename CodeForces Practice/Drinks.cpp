6b5a04d5c62ad7a4b3e9370017ae6e753011573b
ceosss <sswarajsamant@gmail.com>
2020-06-29 21:47:38 +0530

    Drinks

---
+++ CodeForces Practice/Drinks.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    double x, sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        sum += x;
+    }
+
+    cout << fixed << setprecision(12) << sum / n << endl;
+
+    return 0;
+}
