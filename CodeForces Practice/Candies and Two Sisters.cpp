9ea81e17cd8a571870b6ff70f7f56a4dc88fb136
ceosss <sswarajsamant@gmail.com>
2020-07-02 22:53:21 +0530

    Candies and Two Sisters

---
+++ CodeForces Practice/Candies and Two Sisters.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n % 2 == 0)
+            n--;
+        cout << n / 2 << endl;
+    }
+    return 0;
+}
