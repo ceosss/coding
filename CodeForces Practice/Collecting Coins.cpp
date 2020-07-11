e5f3faf0e4e19f01d46eba77736275543bc430a6
ceosss <sswarajsamant@gmail.com>
2020-07-11 20:24:56 +0530

    Collecting Coins

---
+++ CodeForces Practice/Collecting Coins.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, n;
+        cin >> a >> b >> c >> n;
+        int mx = max(a, max(b, c));
+        n -= mx - a;
+        n -= mx - b;
+        n -= mx - c;
+        if (n < 0 || n % 3 != 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
