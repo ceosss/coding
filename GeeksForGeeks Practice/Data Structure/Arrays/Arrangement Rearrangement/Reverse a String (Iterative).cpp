fb3d30444a5b8efcb94c179b62daf4ed50287b4e
ceosss <sswarajsamant@gmail.com>
2020-07-13 15:40:14 +0530

    Restructured the folders

---
+++ GeeksForGeeks Practice/Data Structure/Arrays/Arrangement Rearrangement/Reverse a String (Iterative).cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.length();
+        for (int i = 0; i < n / 2; i++)
+        {
+            char c = s[i];
+            s[i] = s[n - i - 1];
+            s[n - i - 1] = c;
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
