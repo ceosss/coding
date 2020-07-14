2b90af2c107b5a6e0604502f03d38d9e44bccfdb
ceosss <sswarajsamant@gmail.com>
2020-07-14 12:09:38 +0530

    Magical Sticks

---
+++ CodeForces Practice/Magical Sticks.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << (n + 1) / 2 << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
