1a19a6ec85a012784c6e9143fd802ce4dfca9731
ceosss <sswarajsamant@gmail.com>
2020-07-15 16:41:30 +0530

    CopyCopyCopyCopyCopy

---
+++ CodeForces Practice/CopyCopyCopyCopyCopy.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n;
+        set<int> a;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> x;
+            a.insert(x);
+        }
+        cout << a.size() << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
