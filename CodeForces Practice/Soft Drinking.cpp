736f48423e8b03dbe5bf6b9a937151a626abf20c
ceosss <sswarajsamant@gmail.com>
2020-07-16 19:50:11 +0530

    Soft Drinking

---
+++ CodeForces Practice/Soft Drinking.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    int drink = k * l;
+    int toasts = drink / nl;
+    int limes = c * d;
+    int salt = p / np;
+    cout << min(toasts, min(limes, salt)) / n << endl;
+
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
