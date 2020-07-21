6b4e66fdb8675a9f303d8a493555b9308eb49832
ceosss <sswarajsamant@gmail.com>
2020-07-21 20:23:46 +0530

    Puzzle Pieces

---
+++ CodeForces Practice/Puzzle Pieces.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        if ((n == 1 || m == 1) || (n == 2 || m == 2))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
