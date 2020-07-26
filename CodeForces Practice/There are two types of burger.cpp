bcb3dcbd4e5e6099a6f5519212f46b1b447d00ba
ceosss <sswarajsamant@gmail.com>
2020-07-27 00:02:10 +0530

    There Are Two Types Of Burgers

--- CodeForces Practice/There are two types of burger.cpp
+++ CodeForces Practice/There are two types of burger.cpp
@@ -1,45 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-#define forn(i,n) for(ll i=0;i<n;i++)
+#define forn(i, n) for (ll i = 0; i < n; i++)
 int main()
 {
     ll tc;
-    cin>>tc;
- 
-    while(tc--)
+    cin >> tc;
+
+    while (tc--)
     {
-        ll b,p,f,h,c;
-        cin>>b>>p>>f;
-        cin>>h>>c;
-        if(p+f<=b/2)
+        ll b, p, f, h, c;
+        cin >> b >> p >> f;
+        cin >> h >> c;
+        if (p + f <= b / 2)
         {
-            cout<<(h*p)+(c*f)<<endl;
+            cout << (h * p) + (c * f) << endl;
         }
         else
         {
-            if(h>=c && p>=b/2)
+            if (h >= c && p >= b / 2)
             {
-                ll m=b/2;
-                cout<<h*m<<endl;
- 
- 
+                ll m = b / 2;
+                cout << h * m << endl;
             }
-            else if(h>=c && p<=b/2)
+            else if (h >= c && p <= b / 2)
             {
-                cout<<h*p+((b/2)-p)*c<<endl;
+                cout << h * p + ((b / 2) - p) * c << endl;
             }
-            else if(h<=c && f>=b/2)
+            else if (h <= c && f >= b / 2)
             {
-                ll m=b/2;
-                cout<<c*m<<endl;
- 
- 
+                ll m = b / 2;
+                cout << c * m << endl;
             }
-            else if(h<=c && f<=b/2)
+            else if (h <= c && f <= b / 2)
             {
-                cout<<c*f+((b/2)-f)*h<<endl;
+                cout << c * f + ((b / 2) - f) * h << endl;
             }
         }
     }
 }
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
