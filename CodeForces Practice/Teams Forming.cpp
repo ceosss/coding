f2038a495b3f255ca1e6312fb430594a516af75b
ceosss <sswarajsamant@gmail.com>
2020-07-27 20:40:38 +0530

    Teams Forming

---
+++ CodeForces Practice/Teams Forming.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    int problems = 0;
+    for (int i = 0; i < n; i += 2)
+    {
+        problems += abs(a[i] - a[i + 1]);
+    }
+    cout << problems << endl;
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
