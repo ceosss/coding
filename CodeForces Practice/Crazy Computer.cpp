26c4eefd946f2c9fc3791700a5789408ae3f7d8f
ceosss <sswarajsamant@gmail.com>
2020-07-28 21:43:45 +0530

    Crazy Computer

---
+++ CodeForces Practice/Crazy Computer.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int n, c, count = 0, x;
+    cin >> n >> c;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+
+        if (abs(a[i] - a[i - 1]) > c)
+            count = 0;
+        count++;
+    }
+    cout << count << endl;
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
