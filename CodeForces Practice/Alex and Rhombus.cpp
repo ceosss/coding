fab4e24e91eb3d37877776579e06875246f4b973
ceosss <sswarajsamant@gmail.com>
2020-08-03 22:35:04 +0530

    Alex and Rhombus

---
+++ CodeForces Practice/Alex and Rhombus.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+    lli ans = 0;
+    int i;
+    for (i = 1; i < n; i++)
+        ans += 2 * i - 1;
+    cout << ans * 2 + 2 * i - 1 << endl;
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
