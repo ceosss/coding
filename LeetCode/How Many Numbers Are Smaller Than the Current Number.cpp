7c3b45b1d1afe01b9e8ca1fdb84bd0a9bc064e8c
ceosss <sswarajsamant@gmail.com>
2020-08-18 19:48:54 +0530

    How Many Numbers Are Smaller Than the Current Number

---
+++ LeetCode/How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+    {
+        vector<int> b;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j && nums[i] > nums[j])
+                {
+                    count++;
+                }
+            }
+            b.push_back(count);
+        }
+        return b;
+    }
+};
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
