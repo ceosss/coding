c6cf944494c8bfac60818b52ec0240423bf2c442
ceosss <sswarajsamant@gmail.com>
2020-08-22 22:36:07 +0530

    Convert Binary Number in a Linked List to Integet

--- LeetCode/Convert Binary Number in a Linked List to Integer.cpp
+++ LeetCode/Convert Binary Number in a Linked List to Integer.cpp
@@ -1,42 +1,62 @@
 class Solution
 {
 public:
-    int pow(int n, int p)
-    {
-        int num = 1;
-        for (int i = 0; i < p; i++)
-        {
-            num *= n;
-        }
-        return num;
-    }
-    int getLength(ListNode *head)
-    {
-        ListNode *temp = head;
-        int c = 0;
-        while (temp)
-        {
-            c++;
-            temp = temp->next;
-        }
-        return c - 1;
-    }
     int getDecimalValue(ListNode *head)
     {
         ListNode *temp = head;
-        int num = 0, i = getLength(head);
+        int num = 0;
         while (temp)
         {
-            if (temp->val)
-            {
-                num += pow(2, i);
-            }
-            i--;
+            num = (num << 1) + temp->val;
             temp = temp->next;
         }
         return num;
     }
 };
+
+//=======
+//  OR
+//=======
+
+// class Solution
+// {
+// public:
+//     int pow(int n, int p)
+//     {
+//         int num = 1;
+//         for (int i = 0; i < p; i++)
+//         {
+//             num *= n;
+//         }
+//         return num;
+//     }
+//     int getLength(ListNode *head)
+//     {
+//         ListNode *temp = head;
+//         int c = 0;
+//         while (temp)
+//         {
+//             c++;
+//             temp = temp->next;
+//         }
+//         return c - 1;
+//     }
+//     int getDecimalValue(ListNode *head)
+//     {
+//         ListNode *temp = head;
+//         int num = 0, i = getLength(head);
+//         while (temp)
+//         {
+//             if (temp->val)
+//             {
+//                 num += pow(2, i);
+//             }
+//             i--;
+//             temp = temp->next;
+//         }
+//         return num;
+//     }
+// };
 
 //
 //        .--------------.
