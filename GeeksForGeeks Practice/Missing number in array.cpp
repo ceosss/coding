9b280b53e4c75e592d8fba620533841be1d6acee
ceosss <sswarajsamant@gmail.com>
2020-08-22 15:42:37 +0530

    Missing number in array

---
+++ GeeksForGeeks Practice/Missing number in array.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n;
+        int original = 0;
+        int missing = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            original = original ^ i;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            cin >> x;
+            missing = missing ^ x;
+        }
+        cout << (missing ^ original) << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
