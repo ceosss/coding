e96760aae88c3736aea793eadfeda4283b1f9e62
ceosss <sswarajsamant@gmail.com>
2020-08-23 19:27:11 +0530

    Remove Duplicates from Sorted List

---
+++ LeetCode/Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *temp = head;
+        while (temp && temp->next)
+        {
+            if (temp->val == temp->next->val)
+            {
+                ListNode *del = temp->next;
+                if (del->next == NULL)
+                    temp->next = NULL;
+                else
+                    temp->next = del->next;
+                delete del;
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
