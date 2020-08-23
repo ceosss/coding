b20f57553fa6053552053e1852cb3342b10d5cd3
ceosss <sswarajsamant@gmail.com>
2020-08-23 20:19:09 +0530

    Palindrome Linked List

---
+++ LeetCode/Palindrome Linked List.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *temp = head;
+        stack<int> a;
+        while (temp)
+        {
+            a.push(temp->val);
+            temp = temp->next;
+        }
+        while (head != NULL)
+        {
+            int t = a.top();
+            a.pop();
+            if (t != head->val)
+                return false;
+            head = head->next;
+        }
+        return true;
+    }
+};
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
