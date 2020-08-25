59eb3a2be7985a8a251e35c0f07dc5cbd93054eb
ceosss <sswarajsamant@gmail.com>
2020-08-25 22:27:01 +0530

    Fancy Fence

---
+++ CodeForces Practice/Fancy Fence.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        a = 180 - a;
+        if (360 % a == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
