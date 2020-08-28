23482fd3cff9b0f886282db9f0b07dea8e560e64
ceosss <sswarajsamant@gmail.com>
2020-08-28 23:22:59 +0530

    Unique Morse Code Words

---
+++ LeetCode/Unique Morse Code Words.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    int uniqueMorseRepresentations(vector<string> &words)
+    {
+        vector<string> morse{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        set<string> morsed;
+        for (auto i : words)
+        {
+            string morsedWord = "";
+            for (auto j : i)
+            {
+                morsedWord += morse[j - 'a'];
+            }
+            morsed.insert(morsedWord);
+        }
+        return morsed.size();
+    }
+};
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
