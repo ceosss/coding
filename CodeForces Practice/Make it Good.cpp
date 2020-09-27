97d0e5627525dbf16326319d6e511d26fc6fcccc
ceosss <sswarajsamant@gmail.com>
2020-09-27 20:32:48 +0530

    Make it Good

---
+++ CodeForces Practice/Make it Good.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        for (auto &x : a)
+            cin >> x;
+        int res = n - 1;
+
+        while (res > 0 && a[res - 1] >= a[res])
+            --res;
+        while (res > 0 && a[res - 1] <= a[res])
+            --res;
+
+        cout << res << endl;
+    }
+    return 0;
+}
+
+//
+//        .--------------.
+//        | Try First One|
+//        '--------------'
+//                |     .--------------.
+//                |     |              |
+//                V     V              |
+//              .--------------.       |
+//              |      AC.     |<---.  |
+//              '--------------'    |  |
+//              (True)|  |(False)   |  |
+//           .--------'  |          |  |
+//           |           V          |  |
+//           |  .--------------.    |  |
+//           |  |   Try Again  |----'  |
+//           |  '--------------'       |
+//           |                         |
+//           |  .--------------.       |
+//           '->| Try Next One |-------'
+//              '--------------'
+//
