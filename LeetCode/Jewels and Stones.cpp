20e1bc64474d61881d8411e627b29462cbf4ce1d
ceosss <sswarajsamant@gmail.com>
2021-08-03 20:19:44 +0530

    Jewels and Stones

--- LeetCode/Jewels and Stones.cpp
+++ LeetCode/Jewels and Stones.cpp
@@ -1,22 +1,41 @@
 class Solution
 {
 public:
-    int numJewelsInStones(string J, string S)
+    // O(N)
+    int numJewelsInStones(string jewels, string stones)
     {
-        int jLen = J.size();
-        int sLen = S.size();
-        int jewels = 0;
-        for (int i = 0; i < sLen; i++)
+        int count = 0;
+        bool cache[58] = {false};
+
+        for (int i = 0; i < jewels.length(); i++)
+            cache[jewels[i] - 'A'] = true;
+
+        for (int i = 0; i < stones.length(); i++)
         {
-            for (int j = 0; j < jLen; j++)
-            {
-                if (S[i] == J[j])
-                {
-                    jewels++;
-                    break;
-                }
-            }
+            if (cache[stones[i] - 'A'])
+                count++;
         }
-        return jewels;
+
+        return count;
     }
+
+    // O(N^2)
+    // int numJewelsInStones(string J, string S)
+    // {
+    //     int jLen = J.size();
+    //     int sLen = S.size();
+    //     int jewels = 0;
+    //     for (int i = 0; i < sLen; i++)
+    //     {
+    //         for (int j = 0; j < jLen; j++)
+    //         {
+    //             if (S[i] == J[j])
+    //             {
+    //                 jewels++;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return jewels;
+    // }
 };
