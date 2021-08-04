a14ac41b6c53994488903ed62750ea7b446fee9b
ceosss <sswarajsamant@gmail.com>
2021-08-04 14:00:29 +0530

    To Lower Case

--- LeetCode/To Lower Case.cpp
+++ LeetCode/To Lower Case.cpp
@@ -3,10 +3,20 @@ class Solution
 public:
     string toLowerCase(string s)
     {
-        for (int i = 0; i < s.length(); i++)
-            s[i] = tolower(s[i]);
+        int n = s.length();
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                s[i] = s[i] + 32;
+        }
         return s;
     }
+    // string toLowerCase(string s)
+    // {
+    //     for (int i = 0; i < s.length(); i++)
+    //         s[i] = tolower(s[i]);
+    //     return s;
+    // }
 };
 
 //
