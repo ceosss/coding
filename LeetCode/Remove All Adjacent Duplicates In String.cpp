eec38d5844178288016d57e60b43cbaf19cb4bc0
ceosss <sswarajsamant@gmail.com>
2021-08-08 19:26:36 +0530

    Remove All Adjacent Duplicates In String

--- LeetCode/Remove All Adjacent Duplicates In String.cpp
+++ LeetCode/Remove All Adjacent Duplicates In String.cpp
@@ -3,37 +3,53 @@ class Solution
 public:
     string removeDuplicates(string s)
     {
-        stack<char> a;
-        int n = s.length();
-        for (int i = 0; i < n; i++)
+        string result = "";
+        for (auto i : s)
         {
-            if (a.empty())
+            if (!result.length())
             {
-                a.push(s[i]);
+                result += i;
             }
+            else if (result.back() == i)
+                result.pop_back();
             else
-            {
-                char top = a.top();
-                if (s[i] == top)
-                {
-                    a.pop();
-                }
-                else
-                {
-                    a.push(s[i]);
-                }
-            }
+                result += i;
         }
-        string res = "";
-        while (!a.empty())
-        {
-            char top = a.top();
-            res += top;
-            a.pop();
-        }
-        reverse(res.begin(), res.end());
-        return res;
+        return result;
     }
+    // string removeDuplicates(string s)
+    // {
+    //     stack<char> a;
+    //     int n = s.length();
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         if (a.empty())
+    //         {
+    //             a.push(s[i]);
+    //         }
+    //         else
+    //         {
+    //             char top = a.top();
+    //             if (s[i] == top)
+    //             {
+    //                 a.pop();
+    //             }
+    //             else
+    //             {
+    //                 a.push(s[i]);
+    //             }
+    //         }
+    //     }
+    //     string res = "";
+    //     while (!a.empty())
+    //     {
+    //         char top = a.top();
+    //         res += top;
+    //         a.pop();
+    //     }
+    //     reverse(res.begin(), res.end());
+    //     return res;
+    // }
 };
 
 //
