a8cf280ed16535c48a61c87597c66705e56014d8
ceosss <sswarajsamant@gmail.com>
2021-08-24 20:08:34 +0530

    Validate Binary Search Tree

--- LeetCode/Trees/BST/Validate Binary Search Tree.cpp
+++ LeetCode/Trees/BST/Validate Binary Search Tree.cpp
@@ -1,18 +1,38 @@
 class Solution
 {
 public:
-    bool check(TreeNode *root, TreeNode *min, TreeNode *max)
+    void check(TreeNode *root, TreeNode *&prev, bool &res)
     {
         if (!root)
-            return true;
-        if (min && root->val <= min->val)
-            return false;
-        if (max && root->val >= max->val)
-            return false;
-        return check(root->left, min, root) && check(root->right, root, max);
+            return;
+        check(root->left, prev, res);
+        if (prev && prev->val >= root->val)
+        {
+            res = false;
+            return;
+        }
+        prev = root;
+        check(root->right, prev, res);
     }
     bool isValidBST(TreeNode *root)
     {
-        return check(root, NULL, NULL);
+        bool res = true;
+        TreeNode *prev = NULL;
+        check(root, prev, res);
+        return res;
     }
+    // bool check(TreeNode *root, TreeNode *min, TreeNode *max)
+    // {
+    //     if (!root)
+    //         return true;
+    //     if (min && root->val <= min->val)
+    //         return false;
+    //     if (max && root->val >= max->val)
+    //         return false;
+    //     return check(root->left, min, root) && check(root->right, root, max);
+    // }
+    // bool isValidBST(TreeNode *root)
+    // {
+    //     return check(root, NULL, NULL);
+    // }
 };
