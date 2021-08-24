224d948da2121d4f766dd97434a72890a28a3c69
ceosss <sswarajsamant@gmail.com>
2021-08-24 23:21:09 +0530

    All Elements in Two Binary Search Trees

---
+++ LeetCode/Trees/BST/All Elements in Two Binary Search Trees.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void inorder(TreeNode *root, vector<int> &res)
+    {
+        if (!root)
+            return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+
+    vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+    {
+        vector<int> res;
+        inorder(root1, res);
+        inorder(root2, res);
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
