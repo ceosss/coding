70962213b882bfd4aaf201408137dd5ed88b995d
ceosss <sswarajsamant@gmail.com>
2021-08-24 20:02:24 +0530

    Check for BST'

--- GeeksForGeeks Practice/Data Structure/Trees/BST/Check for BST.cpp
+++ GeeksForGeeks Practice/Data Structure/Trees/BST/Check for BST.cpp
@@ -1,19 +1,41 @@
 class Solution
 {
 public:
-    //Function to check whether a Binary Tree is BST or not.
-    bool check(Node *root, Node *min, Node *max)
+    void check(Node *root, Node *&prev, bool &res)
     {
         if (!root)
-            return true;
-        if (min && root->data <= min->data)
-            return false;
-        if (max && root->data >= max->data)
-            return false;
-        return check(root->left, min, root) && check(root->right, root, max);
+            return;
+        check(root->left, prev, res);
+        if (prev && prev->data >= root->data)
+        {
+            res = false;
+            return;
+        }
+        prev = root;
+        check(root->right, prev, res);
     }
     bool isBST(Node *root)
     {
-        return check(root, NULL, NULL);
+        bool res = true;
+        Node *prev = NULL;
+        check(root, prev, res);
+        return res;
     }
 };
+// //Function to check whether a Binary Tree is BST or not.
+// bool check(Node *root, Node *min, Node *max)
+// {
+//     if (!root)
+//         return true;
+//     if (min && root->data <= min->data)
+//         return false;
+//     if (max && root->data >= max->data)
+//         return false;
+//     return check(root->left, min, root) && check(root->right, root, max);
+// }
+// bool isBST(Node *root)
+// {
+//     return check(root, NULL, NULL);
+// }
+}
+;
