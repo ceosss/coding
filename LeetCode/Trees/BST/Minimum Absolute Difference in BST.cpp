5ccefb5e35f5eaf01b378413aa871546d65f2a5b
ceosss <sswarajsamant@gmail.com>
2021-08-26 00:12:27 +0530

    Minimum Absolute Difference in BST

--- LeetCode/Trees/BST/Minimum Absolute Difference in BST.cpp
+++ LeetCode/Trees/BST/Minimum Absolute Difference in BST.cpp
@@ -1,21 +1,38 @@
 class Solution
 {
 public:
-    void inorder(TreeNode *root, vector<int> &res)
+    void find(TreeNode *root, TreeNode *&prev, int &res)
     {
         if (!root)
             return;
-        inorder(root->left, res);
-        res.push_back(root->val);
-        inorder(root->right, res);
+        find(root->left, prev, res);
+        if (prev)
+            res = min(res, abs(root->val - prev->val));
+        prev = root;
+        find(root->right, prev, res);
     }
     int getMinimumDifference(TreeNode *root)
     {
-        vector<int> in;
-        inorder(root, in);
-        int m = INT_MAX;
-        for (int i = 1; i < in.size(); i++)
-            m = min(m, abs(in[i] - in[i - 1]));
-        return m;
+        int res = INT_MAX;
+        TreeNode *prev = NULL;
+        find(root, prev, res);
+        return res;
     }
+    // void inorder(TreeNode *root, vector<int> &res)
+    // {
+    //     if (!root)
+    //         return;
+    //     inorder(root->left, res);
+    //     res.push_back(root->val);
+    //     inorder(root->right, res);
+    // }
+    // int getMinimumDifference(TreeNode *root)
+    // {
+    //     vector<int> in;
+    //     inorder(root, in);
+    //     int m = INT_MAX;
+    //     for (int i = 1; i < in.size(); i++)
+    //         m = min(m, abs(in[i] - in[i - 1]));
+    //     return m;
+    // }
 };
