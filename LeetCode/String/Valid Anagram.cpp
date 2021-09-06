03a8c881f2d187010103ee9aed2bdb922b39e8c0
ceosss <sswarajsamant@gmail.com>
2021-09-06 15:21:41 +0530

    Valid Anagram

---
+++ LeetCode/String/Valid Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<char, int> m;
+
+        for (auto i : s)
+            m[i]++;
+
+        for (auto i : t)
+            m[i]--;
+
+        for (auto &i : m)
+        {
+            if (i.second != 0)
+                return false;
+        }
+        return true;
+    }
+};
