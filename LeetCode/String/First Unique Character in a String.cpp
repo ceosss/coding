426d59afba825a5dd0f6ba2ce195f24fdfb585a3
ceosss <sswarajsamant@gmail.com>
2021-09-10 22:51:41 +0530

    First Unique Character in a String

--- LeetCode/String/First Unique Character in a String.cpp
+++ LeetCode/String/First Unique Character in a String.cpp
@@ -3,18 +3,46 @@ class Solution
 public:
     int firstUniqChar(string s)
     {
-        unordered_map<char, int> m;
-
-        for (auto i : s)
-            m[i]++;
-
+        // TC - o(n)/one pass
+        // SC - o(n)
+        vector<int> cache(26, -1);
         for (int i = 0; i < s.length(); i++)
         {
-            if (m[s[i]] == 1)
+            int x = s[i] - 'a';
+            if (cache[x] == -1)
             {
-                return i;
+                cache[x] = i;
+            }
+            else
+            {
+                cache[x] = -2;
             }
         }
-        return -1;
+        int res = INT_MAX;
+        for (int i = 0; i < 26; i++)
+        {
+            if (cache[i] >= 0)
+            {
+                res = min(res, cache[i]);
+            }
+        }
+
+        return res == INT_MAX ? -1 : res;
+
+        // TC - o(n)
+        // SC - o(n)
+        //     unordered_map<char, int> m;
+
+        //     for (auto i : s)
+        //         m[i]++;
+
+        //     for (int i = 0; i < s.length(); i++)
+        //     {
+        //         if (m[s[i]] == 1)
+        //         {
+        //             return i;
+        //         }
+        //     }
+        //     return -1;
     }
 };
