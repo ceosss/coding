393b763ff0bfa18485c5ab002467ae3503dd1104
ceosss <sswarajsamant@gmail.com>
2021-09-12 10:57:07 +0530

    Move Zeroes

--- LeetCode/Array/Move Zeroes.cpp
+++ LeetCode/Array/Move Zeroes.cpp
@@ -4,16 +4,37 @@ public:
     void moveZeroes(vector<int> &nums)
     {
         // TC - o(n)
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++)
+        // SC - o(1)
+        int n = nums.size();
+        int i = 0, j = 0;
+        while (j < n)
         {
-            if (nums[i] != 0)
+            if (nums[j] != 0)
             {
-                nums[count] = nums[i];
-                count++;
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
             }
         }
-        for (int i = count; i < nums.size(); i++)
-            nums[i] = 0;
+        // TC - o(n)
+        // SC - o(1)
+        // int n = nums.size();
+        // int j = 0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]!=0){
+        //         nums[j++]=nums[i];
+        //     }
+        // }
+        // while(j<n)
+        // {
+        //     nums[j++]=0;
+        // }
     }
 };
