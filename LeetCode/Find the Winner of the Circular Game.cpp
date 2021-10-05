95353e0d662e2d93026b60ff17a5d1c3c7a21f48
ceosss <sswarajsamant@gmail.com>
2021-10-05 22:32:00 +0530

    Find the Winner of the Circular Game

---
+++ LeetCode/Find the Winner of the Circular Game.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        queue<int> q;
+        for (int i = 1; i <= n; i++)
+        {
+            q.push(i);
+        }
+        while (q.size() != 1)
+        {
+            int x = k - 1;
+            while (x--)
+            {
+                q.push(q.front());
+                q.pop();
+            }
+            q.pop();
+        }
+        return q.front();
+    }
+};
