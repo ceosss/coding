3c7a15908edeeb67682884b64ed0501dc76c5afd
ceosss <sswarajsamant@gmail.com>
2021-10-17 18:47:35 +0530

    Copy List with Random Pointer

--- LeetCode/Copy List with Random Pointer.cpp
+++ LeetCode/Copy List with Random Pointer.cpp
@@ -3,21 +3,58 @@ class Solution
 public:
     Node *copyRandomList(Node *head)
     {
-        map<Node *, Node *> m;
-        int i = 0;
-        Node *ptr = head;
-        while (ptr)
+        // TC - o(n)
+        // SC - o(1)
+        Node *temp = head;
+        while (temp)
         {
-            m[ptr] = new Node(ptr->val);
-            ptr = ptr->next;
+            Node *newnode = new Node(temp->val);
+            newnode->next = temp->next;
+            temp->next = newnode;
+            temp = temp->next->next;
         }
-        ptr = head;
-        while (ptr)
+        temp = head;
+        while (temp)
         {
-            m[ptr]->next = m[ptr->next];
-            m[ptr]->random = m[ptr->random];
-            ptr = ptr->next;
+            temp->next->random = temp->random ? temp->random->next : NULL;
+            temp = temp->next->next;
         }
-        return m[head];
+        Node *head2 = NULL;
+        Node *temp2 = NULL;
+        temp = head;
+        while (temp)
+        {
+            Node *node = temp->next;
+            temp->next = node->next;
+            temp = temp->next;
+            if (!head2)
+            {
+                head2 = node;
+                temp2 = head2;
+            }
+            else
+            {
+                temp2->next = node;
+                temp2 = temp2->next;
+            }
+        }
+        return head2;
+        // TC - o(n)
+        // SC - o(n)
+        // unordered_map<Node *, Node *> m;
+        // Node *temp = head;
+        // while (temp)
+        // {
+        //     m[temp] = new Node(temp->val);
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // while (temp)
+        // {
+        //     m[temp]->next = m[temp->next];
+        //     m[temp]->random = m[temp->random];
+        //     temp = temp->next;
+        // }
+        // return m[head];
     }
 };
