09d7b459aaef4883d2a03af68951ce9ece461cf2
ceosss <sswarajsamant@gmail.com>
2021-10-29 23:56:25 +0530

    Word Search

---
+++ LeetCode/Word Search.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+    bool DFS(vector<vector<char>> &board, string word, int i, int j, int n)
+    {
+        if (n == word.size())
+            return true;
+
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n])
+            return false;
+
+        board[i][j] = '0';
+
+        bool status = DFS(board, word, i + 1, j, n + 1) ||
+                      DFS(board, word, i, j + 1, n + 1) ||
+                      DFS(board, word, i - 1, j, n + 1) ||
+                      DFS(board, word, i, j - 1, n + 1);
+
+        board[i][j] = word[n];
+
+        return status;
+    }
+    bool exist(vector<vector<char>> &board, string word)
+    {
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == word[0] &&
+                    DFS(board, word, i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
