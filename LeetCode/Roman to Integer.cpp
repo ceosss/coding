26606ee955ca362539fd06cade7fca37f608e066
ceosss <sswarajsamant@gmail.com>
2021-11-19 21:03:51 +0530

    Roman to Integer

---
+++ LeetCode/Roman to Integer.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    int getVal(char x)
+    {
+        switch (x)
+        {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
+        }
+    }
+    int romanToInt(string s)
+    {
+        int res = 0;
+        int n = s.length();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i == n - 1)
+            {
+                res += getVal(s[i]);
+            }
+            else
+            {
+                if (getVal(s[i]) < getVal(s[i + 1]))
+                {
+                    res -= getVal(s[i]);
+                }
+                else
+                {
+                    res += getVal(s[i]);
+                }
+            }
+        }
+        return res;
+    }
+};
